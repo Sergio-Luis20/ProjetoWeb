55759ccd238d6559f8f1625afd1e7b3bad59308b
Sergio Luis <sergioluisdasilvaruiz05@gmail.com>
2020-06-01 10:13:16 -0300

    SellerFormViewModel and Department select component

---
+++ ProjetoWeb/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using ProjetoWeb.Data;
+using ProjetoWeb.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoWeb.Services
+{
+    public class DepartmentService
+    {
+        private readonly ProjetoWebContext _context;
+
+        public DepartmentService(ProjetoWebContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
