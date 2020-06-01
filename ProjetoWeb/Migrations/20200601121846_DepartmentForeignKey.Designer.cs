7929e7c3d777164a6d79bf7f0ae47f9235e61e4f
Sergio Luis <sergioluisdasilvaruiz05@gmail.com>
2020-06-01 09:27:21 -0300

    Foreign key not null (referential integrity)

--- ProjetoWeb/Migrations/20200601121846_DepartmentForeignKey.Designer.cs
+++ ProjetoWeb/Migrations/20200601121846_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoWeb.Data;
 
 namespace ProjetoWeb.Migrations
 {
     [DbContext(typeof(ProjetoWebContext))]
-    partial class ProjetoWebContextModelSnapshot : ModelSnapshot
+    [Migration("20200601121846_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
